0e652e37327c48526fe3726106c93182fcb8bc65
anzory <andrey.a.zorin@gmail.com>
2022-12-24 12:59:51 +0500

    v.1.6.8

--- src/providers/GpplCompletionItemsProvider.ts
+++ src/providers/GpplCompletionItemsProvider.ts
@@ -7,7 +7,7 @@ import {
   SnippetString,
 } from 'vscode';
 import { gpplComletionsItemsList } from '../util/comletionsItemsList';
-import { IGpplVariable, semanticHelper } from '../util/semanticHelper';
+import { semanticHelper } from '../util/semanticHelper';
 
 export default class GpplCompletionItemsProvider
   implements CompletionItemProvider<CompletionItem>
@@ -22,15 +22,9 @@ export default class GpplCompletionItemsProvider
       _item.kind = item.kind;
 
       if (semanticHelper.isThisSystemVariable(item.label.toString())) {
-        // let _item: CompletionItem = new CompletionItem(item.label.toString());
-        // _item.commitCharacters = new Array<string>(
-        //   item.label.toString().split('')[0]
-        // );
-        // _item.kind = CompletionItemKind.Variable;
         _item.documentation = new MarkdownString('`SolidCAM` system variable');
         _item.detail = '(system variable)';
       } else {
-        // _item.insertText = new SnippetString(item.insertText?.toString());
         _item.documentation = new MarkdownString(
           item.documentation?.toString()
         );
@@ -38,18 +32,6 @@ export default class GpplCompletionItemsProvider
 
       this._gpplComletionsItems.push(_item);
     });
-    // semanticHelper
-    //   .getGpplSystemVariables()
-    //   .forEach((sv: IGpplVariable, idx) => {
-    //     let _item: CompletionItem = new CompletionItem(sv.name);
-    //     _item.insertText = new SnippetString(sv.name);
-    //     _item.documentation = new MarkdownString('`SolidCAM` system variable');
-    //     _item.commitCharacters = new Array<string>(sv.name.split('')[0]);
-    //     _item.detail = '(system variable)';
-    //     _item.kind = CompletionItemKind.Variable;
-
-    //     this._gpplComletionsItems.splice(idx, 1, _item);
-    //   });
   }
   provideCompletionItems(
     document: any,
